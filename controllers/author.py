eccbfc420a01cb8e8cc208125666b33b0f635c2f
Peter Midford <peter.midford@gmail.com>
2014-06-16 18:19:38 -0400

    update some comments

--- controllers/author.py
+++ controllers/author.py
@@ -27,20 +27,27 @@ def show():
 
 
 def update_tool():
+    """
+    Entry point for author tool
+    """
     import author_tools
     print "Entering update_tool controller"
     authors = db().select(db.author.ALL, orderby=db.author.last_name)
     author_sets = author_tools.build_groups(authors)
     return {"authors": author_sets}
 
-
+# implement me
 def update_groups():
-    # honestly don't remember why this is here
-    return dict(message="Hello World")
+    """
+    This is triggered by the submit button on the grouping page
+    and should use the state of any displayed checkboxes to retain
+    or tear down proposed groupings
+    """
+    return {'message': "Not implemented"}
 
 
 def truncate():
-    return dict()
+    return {}
 
 
 def really_truncate():
