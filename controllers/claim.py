4d6d5850ad44ed37b310ff58ef5e4f90794de0c6
Peter Midford <peter.midford@gmail.com>
2014-08-25 21:47:19 -0700

    refining expression graph display

--- controllers/claim.py
+++ controllers/claim.py
@@ -66,12 +66,7 @@ def enter():
         elements = db(db.participant_element.participant == 
                       participant.id).select()
         if elements:
-            element_ids = [element.id for element in elements]  
-            element_labels = [process_p_element(element.id) for element in elements]
-            print "%s; %s" % (str(element_ids),str(element_labels))
-            element_list = element_labels
-            edge_list = [process_p_link(element.id,element_ids) for element in elements]
-            print "links = %s" % str(edge_list)
+            (element_list, edge_list) = process_elements_and_links(elements)
     elif claim_arg:
         claim = db.claim(claim_arg)
         link_table = make_link_table(claim)
@@ -119,6 +114,7 @@ def enter():
             "element_list": element_list,
             "edge_list": edge_list}
 
+
 def get_args(req):
     if req.args(0):
         claim = req.args(0, cast=int)
@@ -157,6 +153,7 @@ def make_link_table(claim):
         result.append(item)
     return result
 
+
 def update_tool():
     """Something to update the representation of participants"""
     result = []
@@ -177,33 +174,48 @@ def update_tool():
                 if len(elements)>0:
                     pass
                 else:
-                    tax_ele_id = None
-                    ana_ele_id = None
-                    sub_ele_id = None
+                    tax_id = None
+                    ana_id = None
+                    sub_id = None
                     if participant.taxon:
                         print "q = %s" % participant.quantification
                         if (participant.quantification == 'some'):
-                            tax_ele_id = insert_participant_element(p_id,some_code)
-                            db.pelement2term.insert(element=tax_ele_id,
-                                                    term=participant.taxon)
+                            tax_id = insert_participant_element(p_id,some_code)
+                            insert_element2term_map(tax_id, participant.taxon)
                         elif (participant.quantification == 'individual'):
-                            tax_ele_id = insert_participant_element(p_id,individual_code)
-                            db.pelement2term.insert(element=tax_ele_id,
-                                                    term=participant.taxon)
+                            ind = lookup_individual(participant.label,
+                                                    participant.taxon,
+                                                    claim.narrative)
+                            if ind is None:
+                                ind = insert_individual(participant.label,
+                                                        participant.taxon,
+                                                        claim.narrative)
+                            tax_id = insert_participant_element(p_id,individual_code)
+                            insert_element2indiv_map(tax_id,ind)
                     if participant.anatomy:
                         print "q = %s" % participant.quantification
                         if (participant.quantification == "some"):
-                            ana_ele_id = insert_participant_element(p_id,some_code)
-                            db.pelement2term.insert(element=ana_ele_id,
-                                                    term=participant.anatomy)
-                    if participant.substrate:
-                        print "q = %s" % participant.quantification
-                        if (participant.quantification == "some"):
-                            sub_ele_id = insert_participant_element(p_id,some_code)
-                            db.pelement2term.insert(element=sub_ele_id,
-                                                    term=participant.anatomy)
+                            ana_id = insert_participant_element(p_id, some_code)
+                            insert_element2term_map(ana_id, participant.anatomy)
+                        elif (participant.quantification == 'individual'):
+                            ana_label = get_term_label(participant.anatomy)
+                            ind_label = ana_label + " of " + participant.label
+                            ind = lookup_individual(ind_label,
+                                                    participant.anatomy,
+                                                    claim.narrative)
+                            if ind is None:
+                                ind = insert_individual(ind_label,
+                                                        participant.anatomy,
+                                                        claim.narrative)
+                            ana_id = insert_participant_element(p_id,individual_code)
+                            insert_element2indiv_map(ana_id, ind)
+                    if participant.substrate:  # assume for now that substrates are always some expressions
+                        print "substrate assumes some quantified"
+                        sub_id = insert_participant_element(p_id,some_code)
+                        insert_element2term_map(sub_id, participant.substrate)
                 result.append((p_id,len(elements))) 
     return {'update_report': result }
+
 
 def get_participant_codes():
     some_code = get_participant_code('some_term')
@@ -213,27 +225,74 @@ def get_participant_codes():
     union_code = get_participant_code('union')
     return (some_code,only_code,individual_code,intersection_code,union_code)
 
+
 def get_participant_code(type_str):
     return db(db.participant_type.label == type_str).select().first().id
 
 
-def insert_participant_element(participant_id,type_id):
+def get_term_label(term_id):
+    return db(db.term.id == term_id).select().first().label
+
+
+def insert_participant_element(participant_id, type_id):
     return db.participant_element.insert(participant=participant_id,type=type_id)
 
 
-def row_count(rows):
-    count = 0
-    for row in rows:
-        count += 1
+def insert_element2term_map(ele_id,term_id):
+    db.pelement2term.insert(element=ele_id, term=term_id)
+
+
+def insert_element2indiv_map(ele_id,indiv_id):
+    db.pelement2individual.insert(element=ele_id, individual=indiv_id)
+
+
+def lookup_individual(label, term, narrative):
+    """first pass at looking up an individual from its label"""
+    # to do, add narrative to this query
+    if label is None:
+        print "no label in lookup"
+        q = db(db.individual.term == term)
+    else:
+        q = db((db.individual.label == label) & (db.individual.term == term))
+    if q.isempty():
+        return None
+    else:
+        rows = q.select();
+        if len(rows) == 1:
+            return rows.first()
+        else:
+            print "Multiple matching individuals"
+            return rows
+
+
+def insert_individual(label, term, narrative):
+    ind = db.individual.insert(label=label, term=term);
+    if narrative:
+        db.individual2narrative.insert(individual=ind,narrative=narrative)
+    return ind
+
+
+def process_elements_and_links(elements):
+    element_ids = [element.id for element in elements]  
+    element_labels = [process_p_element(element.id) for element in elements]
+    print "%s; %s" % (str(element_ids),str(element_labels))
+    element_list = element_labels
+    edge_list = [process_p_link(element.id,element_ids) for element in elements]
+    print "links = %s" % str(edge_list)
+    return (element_list, edge_list)
 
 
 def process_p_element(element_id):
-    terms = db(db.pelement2term.element == element_id).select()
-    if len(terms) > 0:
-        return "term"
-    individuals = db(db.pelement2individual.element == element_id).select()
-    if len(individuals) > 0:
-        return "individual"
+    term_ids = db(db.pelement2term.element == element_id).select()
+    if len(term_ids) > 0:
+        term_id = term_ids[0].term
+        term = db(db.term.id == term_id).select().first()
+        return term.label
+    i_ids = db(db.pelement2individual.element == element_id).select()
+    if len(i_ids) > 0:
+        i_id = i_ids[0].individual
+        individual = db(db.individual.id == i_id).select().first()
+        return individual.label
     return "Neither"
 
 
