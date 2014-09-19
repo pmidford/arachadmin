2456655331d4dd57d9c1c491bd6925444d8019a2
Peter Midford <peter.midford@gmail.com>
2014-09-19 18:56:16 -0400

    converted display labels to use underscores for space (seems to work better with jQuery) and some refactoring inspired by pylint

--- controllers/claim.py
+++ controllers/claim.py
@@ -52,10 +52,9 @@ def show():
 def enter():
     """
     """
-    response.files.append(URL('static/js','d3.js'))
-    response.files.append(URL('static/css', 'd3test.css'))
     element_list = []
     edge_list = []
+    add_d3_files()
     claim_arg, participant_arg = get_args(request)
     if claim_arg and participant_arg:
         claim = db.claim(claim_arg)
@@ -132,16 +131,27 @@ def get_args(req):
         participant = None
     return (claim,participant)
 
+
+def add_d3_files():
+    """adds files related to d3 to the list that is loaded with the entry
+    page.  Needed since d3 is used for element display."""
+    response.files.append(URL('static/js','d3.js'))
+    response.files.append(URL('static/css', 'd3test.css'))
+
+
 def get_primary_participant(claim):
+    """returns the participant row marked as the primary participant
+       for the claim""" 
     rows = db((db.participant2claim.claim == claim.id) &
               (db.participant2claim.participant_index == 1)).select()
-    if len(rows) == 1:
-        return rows[0].participant
+    assert len(rows) == 1
+    return rows[0].participant
 
 
 def make_link_table(claim):
-    """
-    """
+    """generates a set of records that each will correspond to a row of
+       a display table with links to edit pages for each participant in
+       the claim"""
     from claim_tools import make_claim_url, make_participant_url
     rows = db(db.participant2claim.claim == claim.id).select()
     result = []
@@ -190,13 +200,14 @@ def update_tool():
                                 ind = insert_individual(participant.label,
                                                         participant.taxon,
                                                         claim.narrative)
-                            tax_id = insert_participant_element(p_id,individual_code)
+                            tax_id = insert_participant_element(p_id,
+                                                                individual_code)
                             insert_element2indiv_map(tax_id,ind)
                     if participant.anatomy:
                         if (participant.quantification == "some"):
                             ana_id = insert_participant_element(p_id, some_code)
                             insert_element2term_map(ana_id, participant.anatomy)
-                            insert_participant_link(tax_id,ana_id,part_of.id)
+                            insert_participant_link(tax_id, ana_id, part_of.id)
                         elif (participant.quantification == 'individual'):
                             ana_label = get_term_label(participant.anatomy)
                             ind_label = ana_label + " of " + participant.label
@@ -212,7 +223,7 @@ def update_tool():
                             insert_participant_link(tax_id, ana_id, part_of.id)
                     if participant.substrate:  # assume for now that substrates are always some expressions
                         print "substrate assumes some quantified"
-                        sub_id = insert_participant_element(p_id,some_code)
+                        sub_id = insert_participant_element(p_id, some_code)
                         insert_element2term_map(sub_id, participant.substrate)
                 result.append((p_id,len(elements))) 
     return {'update_report': result }
@@ -285,6 +296,8 @@ def lookup_individual(label, term, narrative):
 
 
 def insert_individual(label, term, narrative):
+    """looks up an individual by label and term (type) and
+       updates db to associate it to the narrative"""
     ind = db.individual.insert(label=label, term=term);
     if narrative:
         db.individual2narrative.insert(individual=ind,narrative=narrative)
@@ -314,12 +327,16 @@ def process_p_element(element_id):
     if len(term_ids) > 0:
         term_id = term_ids[0].term
         term = db(db.term.id == term_id).select().first()
-        return term.label
+        label_words = term.label.split(' ')
+        reduced_label = '_'.join(label_words)
+        return reduced_label
     i_ids = db(db.pelement2individual.element == element_id).select()
     if len(i_ids) > 0:
         i_id = i_ids[0].individual
         individual = db(db.individual.id == i_id).select().first()
-        return individual.label
+        label_words = individual.label.split(' ')
+        reduced_label = '_'.join(label_words)
+        return reduced_label
     return "Neither"
 
 
